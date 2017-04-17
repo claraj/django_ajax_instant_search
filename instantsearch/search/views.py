be7505d495acbd6d0f6401d47711d16a1170d1fc
clara <clara@clara.com>
2017-04-17 10:46:17 -0500

    migrations

--- instantsearch/search/views.py
+++ instantsearch/search/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page(request):
+
+    if request.method == 'GET':
+        # Render home page
+
+        return render(request, 'search_page.html')
